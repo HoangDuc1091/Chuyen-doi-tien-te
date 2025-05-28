b932ba59abf6cd3369127cf30b7711429eb74f07
HoangDuc1091 <htduc.btdb1@gmail.com>
2025-05-29 00:05:15 +0700

    Main

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Nhap gia tri dong USD: ");
+        int USD = int.Parse(Console.ReadLine());
+        int VND = USD * 23000;
+        Console.WriteLine("Gia tri dong VND: " + VND);
+    }
+}
